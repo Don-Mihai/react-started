11d56f881ded8b9ea60180e20a3e7bb1b1d76dc9
mikhai.palienko <mikhai.palienko@aliexpress.ru>
2025-07-29 19:14:36 +0300

    feat

--- src/modules/AuthPopUp/AuthPopUp.jsx
+++ src/modules/AuthPopUp/AuthPopUp.jsx
@@ -1,32 +1,71 @@
 import './styles.scss';
 import { useState } from 'react';
 import Modal from '../../components/Modal/Modal';
+import axios from 'axios';
 
 const AuthPopUp = ({ toggleModal, isModalOpen }) => {
-  const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
+
+  const [authValues, setAuthValues] = useState({ email: '', password: '' });
 
   const toggleLogin = () => {
     setIsLogin(!isLogin);
   };
+
+  const onChange = (event) => {
+    const key = event.target.name;
+    const value = event.target.value;
+
+    setAuthValues({ ...authValues, [key]: value });
+  };
+
+  const handleAuth = () => {
+    alert(authValues.email + ' ' + authValues.password);
+  };
+
+  const [registerValues, setRegisterValues] = useState({ name: '', email: '', password: '' });
+
+  const onChangeRegister = (event) => {
+    const key = event.target.name;
+    const value = event.target.value;
+
+    setRegisterValues({ ...registerValues, [key]: value });
+  };
+
+  const handleRegister = async () => {
+    await axios.post('http://localhost:8000/users', registerValues);
+    clearValues(registerValues);
+  };
+
+  //todo: доделать очистку
+  const clearValues = (obj) => {
+    Object.keys(obj).forEach((key) => {
+      obj[key] = '';
+    });
+  };
+
   return (
     <Modal open={isModalOpen} toggleModal={toggleModal} className="auth-popup">
       {isLogin ? (
         <>
           <h3>Вход</h3>
-          <input type="text" placeholder="Email" className="auth-popup__input" />
-          <input type="text" placeholder="Password" className="auth-popup__input" />
+          <input onChange={onChange} value={authValues.email} name="email" type="text" placeholder="Email" className="auth-popup__input" />
+          <input onChange={onChange} value={authValues.password} name="password" type="text" placeholder="Password" className="auth-popup__input" />
           <div className="auth-popup__link" onClick={toggleLogin}>
             Нет аккаунта? Зарегистрироваться
           </div>
+          <button onClick={handleAuth}>Войти</button>
         </>
       ) : (
         <>
           <h3>Регистрация</h3>
-          <input type="text" placeholder="Name" className="auth-popup__input" />
-          <input type="text" placeholder="Email" className="auth-popup__input" />
+          <input onChange={onChangeRegister} value={registerValues.name} name="name" type="text" placeholder="Name" className="auth-popup__input" />
+          <input onChange={onChangeRegister} value={registerValues.email} name="email" type="text" placeholder="Email" className="auth-popup__input" />
+          <input onChange={onChangeRegister} value={registerValues.password} name="password" type="text" placeholder="Password" className="auth-popup__input" />
           <div className="auth-popup__link" onClick={toggleLogin}>
             Уже есть аккаунт? Войти
           </div>
+          <button onClick={handleRegister}>Регистрация</button>
         </>
       )}
     </Modal>
